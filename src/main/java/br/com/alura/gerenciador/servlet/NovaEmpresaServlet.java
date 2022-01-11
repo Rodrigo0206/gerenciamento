6870cb9c5a5cc8e268f9f4c54bdbdb5a32a44678
unknown <rodrigo.domingues.87@hotmail.com>
2022-01-11 13:44:33 -0300

    Enviando parametros

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,7 +1,10 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +18,11 @@ public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Cadastrando nova empresa");
+		String nomeEmpresa = request.getParameter("nome");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!!</body></html>");
+		
 	}
 
 }
