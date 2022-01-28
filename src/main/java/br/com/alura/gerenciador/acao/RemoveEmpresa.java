51ab994b3aebe1f8256f3022fa8af8fab25ac3d7
unknown <rodrigo.domingues.87@hotmail.com>
2022-01-28 11:43:06 -0300

    Centralizando o Redirecionamento

--- src/main/java/br/com/alura/gerenciador/acao/RemoveEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/acao/RemoveEmpresa.java
@@ -9,19 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.modelo.Banco;
 
 public class RemoveEmpresa {
-	
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("acao removendo empresa");
-		
-		String paramId = request.getParameter("id");
-		Integer id = Integer.valueOf(paramId);
-		
-		System.out.println(id);
-		
-		Banco banco = new Banco();
-		banco.removeEmpresa(id);
-		
-		response.sendRedirect("entrada?acao=ListaEmpresas");
-	}
+
+    public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("removendo empresa");
+
+        String paramId = request.getParameter("id");
+        Integer id = Integer.valueOf(paramId);
+
+        System.out.println(id);
+
+        Banco banco = new Banco();
+        banco.removeEmpresa(id);
+
+        return "redirect:entrada?acao=ListaEmpresas";
+    } 	
 }
