9aa8dc397c61d63bf9d8cd1eb5a8f4fbfc8a7492
unknown <rodrigo.domingues.87@hotmail.com>
2022-02-01 18:32:47 -0300

    Modelo e view

--- src/main/java/br/com/alura/gerenciador/acao/LoginForm.java
+++ src/main/java/br/com/alura/gerenciador/acao/LoginForm.java
@@ -6,17 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Login implements Acao {
+public class LoginForm implements Acao {
 
 	@Override
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		String login = request.getParameter("login");
-		String senha = request.getParameter("senha");
-		
-		System.out.println("logando" + login);
-		return "redirect:entrada?acao=ListaEmpresas";
+		// TODO Auto-generated method stub
+		return "forward:formLogin.jsp";
 	}
 
 }
