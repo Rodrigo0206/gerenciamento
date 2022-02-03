47d603c019bfdf4ff9d298ea638b7b5e3dce1d0c
unknown <rodrigo.domingues.87@hotmail.com>
2022-02-03 17:50:34 -0300

    Ordem de execucao

--- src/main/java/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ src/main/java/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/entrada")
+//@WebFilter("/entrada")
 public class AutorizacaoFilter extends HttpFilter implements Filter {
        
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
